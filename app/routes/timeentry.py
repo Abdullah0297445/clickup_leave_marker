6f589fb282a8803136c3efe0db69f656a8343deb
Abdullah <abdullah.ajmal.7@hotmail.com>
2024-06-13 13:05:50 +0500

    Add AWS credentials to .env.example and update timeentry route
    The .env.example file is updated to include AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY as environment variables. The POST route in app/routes/timeentry.py is now expecting a secret to verify the webhook. Also, a skeleton structure for handling logic after verifying the webhook has been added.

--- app/routes/timeentry.py
+++ app/routes/timeentry.py
@@ -1,12 +1,26 @@
 from fastapi import APIRouter
 from fastapi import status
+from pydantic import SecretStr
+import os
 
 router = APIRouter()
 
 
-@router.post("/")
-async def create_entry(status_code=status.HTTP_204_NO_CONTENT) -> None:
+@router.post("/", status_code=status.HTTP_204_NO_CONTENT)
+async def create_entry(secret: SecretStr, webhook_data) -> None:
     """
     Create a time entry from 9AM - 5PM on the day of the leave.
     """
+
+    print(webhook_data)
+
+    # 1. Verify the secret
+    if not os.environ.get("WEBHOOK_SECRET") == secret:
+        print("Incorrect credentials")
+        return
+
+    # 2. Extract User and their date of leave
+    print("User and their leave date extracted")
+
+    # 3. Track the time for the user for the date via ClickUp API
     print("Time tracked")
